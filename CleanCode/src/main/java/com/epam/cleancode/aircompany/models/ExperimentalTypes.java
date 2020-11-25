94fd9f1ae9c6245532d3d9f5f1251e88fe2e9df5
Pavel Sharuba <mynameisfich@gmail.com>
2020-11-25 08:42:15 +0300

    add cleancode

---
+++ CleanCode/src/main/java/com/epam/cleancode/aircompany/models/ExperimentalTypes.java
@@ -0,0 +1,5 @@
+package com.epam.cleancode.aircompany.models;
+
+public enum ExperimentalTypes {
+    LIFTING_BODY, HYPERSONIC, HIGH_ALTITUDE, VTOL
+}
---
+++ CleanCode/src/main/java/com/epam/cleancode/aircompany/models/MilitaryType.java
@@ -0,0 +1,5 @@
+package com.epam.cleancode.aircompany.models;
+
+public enum MilitaryType {
+    FIGHTER, BOMBER, TRANSPORT
+}
