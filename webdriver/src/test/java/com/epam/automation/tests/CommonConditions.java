7d5cb329c750d73a7f10ce00aec97c0bece17744
Pavel Sharuba <mynameisfich@gmail.com>
2021-01-05 22:09:28 +0300

    add more logger

--- webdriver/src/test/java/com/epam/automation/tests/CommonConditions.java
+++ webdriver/src/test/java/com/epam/automation/tests/CommonConditions.java
@@ -2,6 +2,8 @@ package com.epam.automation.tests;
 
 import com.epam.automation.driver.DriverSingleton;
 import com.epam.automation.util.TestListener;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -10,15 +12,18 @@ import org.testng.annotations.Listeners;
 @Listeners({TestListener.class})
 public class CommonConditions {
     protected WebDriver webDriver;
+    private Logger log = LogManager.getRootLogger();
 
-    @BeforeMethod()
+    @BeforeMethod(alwaysRun = true)
     public void browserSetup() {
         webDriver = DriverSingleton.getDriver();
+        log.info("Got driver");
     }
 
     @AfterMethod(alwaysRun = true)
     public void closeBrowser() {
         DriverSingleton.closeDriver();
         webDriver = null;
+        log.info("Closed driver");
     }
 }
